5b8e468b40a7bb9a4b9424b2a8d31394892d7feb
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-03 15:42:02 +0200

    up 01 hello world

--- 01HelloWorld/Form1.Designer.cs
+++ 01HelloWorld/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _01HelloWorld
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(811, 115);
+            Name = "frmMain";
+            Text = "Premiers pas en Winform";
+            ResumeLayout(false);
         }
 
         #endregion
