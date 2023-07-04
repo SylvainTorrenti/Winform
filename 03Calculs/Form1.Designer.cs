f41773a21db9450b5f431d66d7ebecedc8d3b1aa
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-04 10:49:48 +0200

    exo 03

--- 03Calculs/Form1.Designer.cs
+++ 03Calculs/Form1.Designer.cs
@@ -32,11 +32,14 @@
             tbNb1 = new TextBox();
             tbNb2 = new TextBox();
             lbAdition = new Label();
+            tbResult = new TextBox();
+            btEgal = new Button();
+            btReset = new Button();
             SuspendLayout();
             // 
             // tbNb1
             // 
-            tbNb1.Location = new Point(310, 24);
+            tbNb1.Location = new Point(277, 24);
             tbNb1.Name = "tbNb1";
             tbNb1.Size = new Size(502, 23);
             tbNb1.TabIndex = 0;
@@ -44,7 +47,7 @@
             // 
             // tbNb2
             // 
-            tbNb2.Location = new Point(310, 78);
+            tbNb2.Location = new Point(277, 78);
             tbNb2.Name = "tbNb2";
             tbNb2.Size = new Size(502, 23);
             tbNb2.TabIndex = 1;
@@ -54,17 +57,50 @@
             // 
             lbAdition.AutoSize = true;
             lbAdition.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            lbAdition.Location = new Point(269, 76);
+            lbAdition.Location = new Point(236, 76);
             lbAdition.Name = "lbAdition";
             lbAdition.Size = new Size(25, 25);
             lbAdition.TabIndex = 2;
             lbAdition.Text = "+";
+            // 
+            // tbResult
+            // 
+            tbResult.Location = new Point(277, 134);
+            tbResult.Name = "tbResult";
+            tbResult.Size = new Size(502, 23);
+            tbResult.TabIndex = 3;
+            tbResult.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btEgal
+            // 
+            btEgal.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btEgal.Location = new Point(236, 134);
+            btEgal.Name = "btEgal";
+            btEgal.Size = new Size(33, 23);
+            btEgal.TabIndex = 4;
+            btEgal.Text = "=";
+            btEgal.UseVisualStyleBackColor = true;
+            btEgal.Click += btEgal_Click;
+            // 
+            // btReset
+            // 
+            btReset.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btReset.Location = new Point(796, 24);
+            btReset.Name = "btReset";
+            btReset.Size = new Size(36, 23);
+            btReset.TabIndex = 5;
+            btReset.Text = "CE";
+            btReset.UseVisualStyleBackColor = true;
+            btReset.Click += btReset_Click;
             // 
             // frmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(844, 200);
+            Controls.Add(btReset);
+            Controls.Add(btEgal);
+            Controls.Add(tbResult);
             Controls.Add(lbAdition);
             Controls.Add(tbNb2);
             Controls.Add(tbNb1);
@@ -72,6 +108,7 @@
             Name = "frmMain";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Calcul";
+            Load += frmMain_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -81,5 +118,8 @@
         private TextBox tbNb1;
         private TextBox tbNb2;
         private Label lbAdition;
+        private TextBox tbResult;
+        private Button btEgal;
+        private Button btReset;
     }
 }
--- 03Calculs/Form1.cs
+++ 03Calculs/Form1.cs
@@ -6,5 +6,41 @@ namespace _03Calculs
         {
             InitializeComponent();
         }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            ToolTip tpReset = new ToolTip();
+            tpReset.AutoPopDelay = 0;
+            tpReset.InitialDelay = 0;
+            tpReset.ReshowDelay = 0;
+            tpReset.ShowAlways = true;
+            tpReset.SetToolTip(this.btReset, "Reset");
+
+            ToolTip tpResult = new ToolTip();
+            tpResult.AutoPopDelay = 0;
+            tpResult.InitialDelay = 0;
+            tpResult.ReshowDelay = 0;
+            tpResult.ShowAlways = true;
+            tpResult.SetToolTip(this.btEgal, "Resultat");
+
+        }
+
+        private void btEgal_Click(object sender, EventArgs e)
+        {
+            int nb1 = int.Parse(tbNb1.Text);
+            int nb2 = int.Parse(tbNb2.Text);
+            int result = nb1 + nb2;
+
+            tbResult.Text = result.ToString();
+        }
+
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            tbNb1.Clear();
+            tbNb2.Clear();
+            tbResult.Clear();
+            tbNb1.Focus();
+        }
+
+
     }
 }
