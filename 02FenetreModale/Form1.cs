aecc643b1fa860d48997496f467a867679d4fedc
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-04 10:15:42 +0200

    exo 02

--- 02FenetreModale/Form1.cs
+++ 02FenetreModale/Form1.cs
@@ -9,8 +9,22 @@ namespace _02FenetreModale
 
         private void btFrmFille_Click(object sender, EventArgs e)
         {
-            
-
+            ShowMyDialogBox();
+        }
+        public void ShowMyDialogBox()
+        {
+            frmFille frmFille = new frmFille();
+            // Show testDialog as a modal dialog and determine if DialogResult = OK.
+            if (frmFille.ShowDialog(this) == DialogResult.OK)
+            {
+                // Read the contents of testDialog's TextBox.
+                this.tbTitle.Text = frmFille.Text;
+            }
+            else
+            {
+                this.tbTitle.Text = "";
+            }
+            frmFille.Dispose();
         }
     }
 }
