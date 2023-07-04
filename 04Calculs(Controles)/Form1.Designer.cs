463b866a2204f5ba218085c1da3ddc3d93a31885
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-04 11:18:57 +0200

    exo 04

---
+++ 04Calculs(Controles)/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace _04Calculs_Controles_
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            tbNb1 = new TextBox();
+            tbNb2 = new TextBox();
+            tbResult = new TextBox();
+            lbAdition = new Label();
+            btReset = new Button();
+            btResult = new Button();
+            SuspendLayout();
+            // 
+            // tbNb1
+            // 
+            tbNb1.Location = new Point(313, 28);
+            tbNb1.Name = "tbNb1";
+            tbNb1.Size = new Size(300, 23);
+            tbNb1.TabIndex = 0;
+            tbNb1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // tbNb2
+            // 
+            tbNb2.Location = new Point(313, 72);
+            tbNb2.Name = "tbNb2";
+            tbNb2.Size = new Size(300, 23);
+            tbNb2.TabIndex = 1;
+            tbNb2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // tbResult
+            // 
+            tbResult.Location = new Point(313, 147);
+            tbResult.Name = "tbResult";
+            tbResult.Size = new Size(300, 23);
+            tbResult.TabIndex = 2;
+            tbResult.TextAlign = HorizontalAlignment.Right;
+            // 
+            // lbAdition
+            // 
+            lbAdition.AutoSize = true;
+            lbAdition.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbAdition.Location = new Point(272, 70);
+            lbAdition.Name = "lbAdition";
+            lbAdition.Size = new Size(25, 25);
+            lbAdition.TabIndex = 3;
+            lbAdition.Text = "+";
+            // 
+            // btReset
+            // 
+            btReset.Location = new Point(650, 27);
+            btReset.Name = "btReset";
+            btReset.Size = new Size(32, 23);
+            btReset.TabIndex = 4;
+            btReset.Text = "CE";
+            btReset.UseVisualStyleBackColor = true;
+            btReset.Click += btReset_Click;
+            // 
+            // btResult
+            // 
+            btResult.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btResult.Location = new Point(261, 147);
+            btResult.Name = "btResult";
+            btResult.Size = new Size(27, 23);
+            btResult.TabIndex = 5;
+            btResult.Text = "=";
+            btResult.UseVisualStyleBackColor = true;
+            btResult.Click += btResult_Click;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(742, 233);
+            Controls.Add(btResult);
+            Controls.Add(btReset);
+            Controls.Add(lbAdition);
+            Controls.Add(tbResult);
+            Controls.Add(tbNb2);
+            Controls.Add(tbNb1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmMain";
+            Text = "Calcul";
+            Load += frmMain_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbNb1;
+        private TextBox tbNb2;
+        private TextBox tbResult;
+        private Label lbAdition;
+        private Button btReset;
+        private Button btResult;
+    }
+}
---
+++ 04Calculs(Controles)/Form1.cs
@@ -0,0 +1,66 @@
+namespace _04Calculs_Controles_
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            ToolTip tpReset = new ToolTip();
+            tpReset.AutoPopDelay = 0;
+            tpReset.InitialDelay = 0;
+            tpReset.ReshowDelay = 0;
+            tpReset.ShowAlways = true;
+            tpReset.SetToolTip(this.btReset, "Reset");
+
+            ToolTip tpResult = new ToolTip();
+            tpResult.AutoPopDelay = 0;
+            tpResult.InitialDelay = 0;
+            tpResult.ReshowDelay = 0;
+            tpResult.ShowAlways = true;
+            tpResult.SetToolTip(this.btResult, "Resultat");
+
+            ToolTip tbNb1 = new ToolTip();
+            tbNb1.AutoPopDelay = 0;
+            tbNb1.InitialDelay = 0;
+            tbNb1.ReshowDelay = 0;
+            tbNb1.ShowAlways = true;
+            tbNb1.SetToolTip(this.tbNb1, "Le 1er nombre");
+
+            ToolTip tbNb2 = new ToolTip();
+            tbNb2.AutoPopDelay = 0;
+            tbNb2.InitialDelay = 0;
+            tbNb2.ReshowDelay = 0;
+            tbNb2.ShowAlways = true;
+            tbNb2.SetToolTip(this.tbNb2, "Le 2eme nombre");
+
+            ToolTip tbResult = new ToolTip();
+            tbResult.AutoPopDelay = 0;
+            tbResult.InitialDelay = 0;
+            tbResult.ReshowDelay = 0;
+            tbResult.ShowAlways = true;
+            tbResult.SetToolTip(this.tbResult, "Le resultat");
+        }
+
+        private void btResult_Click(object sender, EventArgs e)
+        {
+            int nb1 = int.Parse(tbNb1.Text);
+            int nb2 = int.Parse(tbNb2.Text);
+            int result = nb1 + nb2;
+
+            tbResult.Text = result.ToString();
+        }
+
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            tbNb1.Clear();
+            tbNb2.Clear();
+            tbResult.Clear();
+            tbNb1.Focus();
+        }
+
+
+    }
+}
