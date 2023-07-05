412e4e83c52651679109cfff56e7b05afa1683c6
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-05 16:23:33 +0200

    exo 09 1.0

---
+++ 09Age(settings)/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace _09Age_settings_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            lbName = new Label();
+            lbBirthday = new Label();
+            tbName = new TextBox();
+            tbBirthday = new TextBox();
+            btClose = new Button();
+            btYear = new Button();
+            SuspendLayout();
+            // 
+            // lbName
+            // 
+            lbName.AutoSize = true;
+            lbName.Location = new Point(35, 33);
+            lbName.Name = "lbName";
+            lbName.Size = new Size(34, 15);
+            lbName.TabIndex = 0;
+            lbName.Text = "Nom";
+            // 
+            // lbBirthday
+            // 
+            lbBirthday.AutoSize = true;
+            lbBirthday.Location = new Point(35, 68);
+            lbBirthday.Name = "lbBirthday";
+            lbBirthday.Size = new Size(101, 15);
+            lbBirthday.TabIndex = 1;
+            lbBirthday.Text = "Date de naissance";
+            // 
+            // tbName
+            // 
+            tbName.CharacterCasing = CharacterCasing.Upper;
+            tbName.Location = new Point(203, 33);
+            tbName.Name = "tbName";
+            tbName.Size = new Size(199, 23);
+            tbName.TabIndex = 2;
+            tbName.TextChanged += tbName_TextChanged;
+            // 
+            // tbBirthday
+            // 
+            tbBirthday.Location = new Point(203, 65);
+            tbBirthday.Name = "tbBirthday";
+            tbBirthday.Size = new Size(199, 23);
+            tbBirthday.TabIndex = 3;
+            tbBirthday.TextChanged += tbBirthday_TextChanged;
+            // 
+            // btClose
+            // 
+            btClose.Image = Properties.Resources.star_alt_icon_34347;
+            btClose.ImageAlign = ContentAlignment.MiddleLeft;
+            btClose.Location = new Point(35, 166);
+            btClose.Name = "btClose";
+            btClose.Size = new Size(101, 59);
+            btClose.TabIndex = 4;
+            btClose.Text = "Fermer";
+            btClose.TextAlign = ContentAlignment.MiddleRight;
+            btClose.UseVisualStyleBackColor = true;
+            btClose.Click += btClose_Click;
+            // 
+            // btYear
+            // 
+            btYear.Enabled = false;
+            btYear.Image = Properties.Resources.runner_sport_run_olympics_olympic_athletics_icon_123810;
+            btYear.ImageAlign = ContentAlignment.MiddleLeft;
+            btYear.Location = new Point(255, 166);
+            btYear.Name = "btYear";
+            btYear.Size = new Size(101, 59);
+            btYear.TabIndex = 5;
+            btYear.Text = "Calculer";
+            btYear.TextAlign = ContentAlignment.MiddleRight;
+            btYear.UseVisualStyleBackColor = true;
+            btYear.Click += btYear_Click;
+            // 
+            // Form1
+            // 
+            AcceptButton = btYear;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btClose;
+            ClientSize = new Size(433, 262);
+            Controls.Add(btYear);
+            Controls.Add(btClose);
+            Controls.Add(tbBirthday);
+            Controls.Add(tbName);
+            Controls.Add(lbBirthday);
+            Controls.Add(lbName);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbName;
+        private Label lbBirthday;
+        private TextBox tbName;
+        private TextBox tbBirthday;
+        private Button btClose;
+        private Button btYear;
+    }
+}
---
+++ 09Age(settings)/Form1.cs
@@ -0,0 +1,66 @@
+using System.Text.RegularExpressions;
+
+namespace _09Age_settings_
+{
+    public partial class Form1 : Form
+    {
+        const string TITRE = "Age";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.tbBirthday.Text = Properties.Settings.Default.DtNaiss;
+            this.tbName.Text = Properties.Settings.Default.Nom;
+        }
+        private void visible()
+        {
+            var regex = new Regex("^[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{4}$");
+            if ((tbName.Text != "") && (tbBirthday.Text != ""))
+            {
+                if (regex.IsMatch(tbBirthday.Text))
+                {
+                    int result = DateTime.Compare(Convert.ToDateTime(tbBirthday.Text), DateTime.Now);
+                    if (result < 0)
+                    {
+
+                        btYear.Enabled = true;
+                    }
+                }
+            }
+        }
+
+        private void btYear_Click(object sender, EventArgs e)
+        {
+            var name = tbName.Text;
+            var today = DateTime.Today;
+            var birthday = Convert.ToDateTime(tbBirthday.Text);
+            var year = today.Year - birthday.Year;
+            MessageBox.Show("bonjour " + name + ", vous avez " + year + " ans", TITRE, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void tbName_TextChanged(object sender, EventArgs e)
+        {
+            visible();
+        }
+
+        private void tbBirthday_TextChanged(object sender, EventArgs e)
+        {
+            visible();
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.Nom = tbName.Text;
+            Properties.Settings.Default.DtNaiss = tbBirthday.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
