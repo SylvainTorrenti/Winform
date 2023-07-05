64d30ffdcce704db33d596656ad71476843c3d71
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-05 16:42:27 +0200

    exo 10 1.1

--- Age(DateTimePicker)/Form1.Designer.cs
+++ Age(DateTimePicker)/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Age_DateTimePicker_
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,18 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            lbName = new Label();
+            lbBirthday = new Label();
+            tbName = new TextBox();
+            dtpYear = new DateTimePicker();
+            btCancel = new Button();
+            btYear = new Button();
             SuspendLayout();
             // 
-            // Form1
+            // lbName
+            // 
+            lbName.AutoSize = true;
+            lbName.Location = new Point(55, 21);
+            lbName.Name = "lbName";
+            lbName.Size = new Size(34, 15);
+            lbName.TabIndex = 0;
+            lbName.Text = "Nom";
+            // 
+            // lbBirthday
+            // 
+            lbBirthday.AutoSize = true;
+            lbBirthday.Location = new Point(55, 59);
+            lbBirthday.Name = "lbBirthday";
+            lbBirthday.Size = new Size(101, 15);
+            lbBirthday.TabIndex = 1;
+            lbBirthday.Text = "Date de naissance";
+            // 
+            // tbName
+            // 
+            tbName.CharacterCasing = CharacterCasing.Upper;
+            tbName.Location = new Point(222, 13);
+            tbName.Name = "tbName";
+            tbName.Size = new Size(253, 23);
+            tbName.TabIndex = 2;
+            // 
+            // dtpYear
+            // 
+            dtpYear.Location = new Point(222, 53);
+            dtpYear.MaxDate = new DateTime(2023, 7, 5, 0, 0, 0, 0);
+            dtpYear.Name = "dtpYear";
+            dtpYear.Size = new Size(253, 23);
+            dtpYear.TabIndex = 3;
+            dtpYear.Value = new DateTime(2023, 7, 5, 0, 0, 0, 0);
+            // 
+            // btCancel
+            // 
+            btCancel.Image = Properties.Resources.star_alt_icon_34347;
+            btCancel.ImageAlign = ContentAlignment.MiddleLeft;
+            btCancel.Location = new Point(55, 149);
+            btCancel.Name = "btCancel";
+            btCancel.Size = new Size(101, 68);
+            btCancel.TabIndex = 4;
+            btCancel.Text = "Fermer";
+            btCancel.TextAlign = ContentAlignment.MiddleRight;
+            btCancel.UseVisualStyleBackColor = true;
+            // 
+            // btYear
+            // 
+            btYear.Enabled = false;
+            btYear.Image = Properties.Resources.runner_sport_run_olympics_olympic_athletics_icon_123810;
+            btYear.ImageAlign = ContentAlignment.MiddleLeft;
+            btYear.Location = new Point(293, 149);
+            btYear.Name = "btYear";
+            btYear.Size = new Size(101, 68);
+            btYear.TabIndex = 5;
+            btYear.Text = "Calculer";
+            btYear.TextAlign = ContentAlignment.MiddleRight;
+            btYear.UseVisualStyleBackColor = true;
+            btYear.Click += btYear_Click;
+            // 
+            // frmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(512, 241);
-            Name = "Form1";
-            Text = "Form1";
+            Controls.Add(btYear);
+            Controls.Add(btCancel);
+            Controls.Add(dtpYear);
+            Controls.Add(tbName);
+            Controls.Add(lbBirthday);
+            Controls.Add(lbName);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Age";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbName;
+        private Label lbBirthday;
+        private TextBox tbName;
+        private DateTimePicker dtpYear;
+        private Button btCancel;
+        private Button btYear;
     }
 }
--- Age(DateTimePicker)/Form1.cs
+++ Age(DateTimePicker)/Form1.cs
@@ -1,10 +1,22 @@
+using System.Text.RegularExpressions;
+
 namespace Age_DateTimePicker_
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        const string TITRE = "Age";
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btYear_Click(object sender, EventArgs e)
+        {
+            var name = tbName.Text;
+            var today = DateTime.Today;
+            var birthday = Convert.ToDateTime
+            var year = today - dtpYear;
+            MessageBox.Show("bonjour " + name + ", vous avez " + year + " ans", TITRE, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
