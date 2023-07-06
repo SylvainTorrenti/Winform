552be36223e88cc27cc82e8bf4008eb42114a7f7
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-06 13:52:10 +0200

    exo 15 1.1

--- 15Image(PictureBox)/Form1.Designer.cs
+++ 15Image(PictureBox)/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _15Image_PictureBox_
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            btImage = new Button();
+            tbPath = new TextBox();
+            libPath = new ListBox();
+            btAdd = new Button();
+            btClear = new Button();
+            btDelete = new Button();
+            pImage = new Panel();
+            pbImage = new PictureBox();
+            pImage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pbImage).BeginInit();
+            SuspendLayout();
+            // 
+            // btImage
+            // 
+            btImage.Image = Properties.Resources.photo_photography_image_picture_108525;
+            btImage.Location = new Point(12, 12);
+            btImage.Name = "btImage";
+            btImage.Size = new Size(39, 32);
+            btImage.TabIndex = 0;
+            btImage.UseVisualStyleBackColor = true;
+            btImage.Click += btImage_Click;
+            // 
+            // tbPath
+            // 
+            tbPath.Location = new Point(57, 18);
+            tbPath.Name = "tbPath";
+            tbPath.Size = new Size(568, 23);
+            tbPath.TabIndex = 1;
+            // 
+            // libPath
+            // 
+            libPath.FormattingEnabled = true;
+            libPath.ItemHeight = 15;
+            libPath.Location = new Point(12, 50);
+            libPath.Name = "libPath";
+            libPath.Size = new Size(613, 169);
+            libPath.TabIndex = 2;
+            libPath.SelectedIndexChanged += libPath_SelectedIndexChanged;
+            // 
+            // btAdd
+            // 
+            btAdd.Enabled = false;
+            btAdd.Image = Properties.Resources.addition_sign_icon_icons_com_73100;
+            btAdd.ImageAlign = ContentAlignment.MiddleRight;
+            btAdd.Location = new Point(644, 50);
+            btAdd.Name = "btAdd";
+            btAdd.Size = new Size(144, 43);
+            btAdd.TabIndex = 4;
+            btAdd.Text = "Ajouter";
+            btAdd.TextAlign = ContentAlignment.MiddleLeft;
+            btAdd.UseVisualStyleBackColor = true;
+            btAdd.Click += btAdd_Click;
+            // 
+            // btClear
+            // 
+            btClear.Enabled = false;
+            btClear.Image = Properties.Resources.filesystems_garbage_full_670;
+            btClear.ImageAlign = ContentAlignment.MiddleRight;
+            btClear.Location = new Point(644, 176);
+            btClear.Name = "btClear";
+            btClear.Size = new Size(144, 43);
+            btClear.TabIndex = 5;
+            btClear.Text = "Vider";
+            btClear.TextAlign = ContentAlignment.MiddleLeft;
+            btClear.UseVisualStyleBackColor = true;
+            btClear.Click += btClear_Click;
+            // 
+            // btDelete
+            // 
+            btDelete.Enabled = false;
+            btDelete.Image = Properties.Resources.delete_delete_exit_1577;
+            btDelete.ImageAlign = ContentAlignment.MiddleRight;
+            btDelete.Location = new Point(644, 113);
+            btDelete.Name = "btDelete";
+            btDelete.Size = new Size(144, 43);
+            btDelete.TabIndex = 6;
+            btDelete.Text = "Supprimer";
+            btDelete.TextAlign = ContentAlignment.MiddleLeft;
+            btDelete.UseVisualStyleBackColor = true;
+            btDelete.Click += btDelete_Click;
+            // 
+            // pImage
+            // 
+            pImage.AutoScroll = true;
+            pImage.BorderStyle = BorderStyle.FixedSingle;
+            pImage.Controls.Add(pbImage);
+            pImage.Location = new Point(12, 225);
+            pImage.Name = "pImage";
+            pImage.Size = new Size(776, 213);
+            pImage.TabIndex = 7;
+            // 
+            // pbImage
+            // 
+            pbImage.Dock = DockStyle.Fill;
+            pbImage.Location = new Point(0, 0);
+            pbImage.Name = "pbImage";
+            pbImage.Size = new Size(774, 211);
+            pbImage.SizeMode = PictureBoxSizeMode.AutoSize;
+            pbImage.TabIndex = 0;
+            pbImage.TabStop = false;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pImage);
+            Controls.Add(btDelete);
+            Controls.Add(btClear);
+            Controls.Add(btAdd);
+            Controls.Add(libPath);
+            Controls.Add(tbPath);
+            Controls.Add(btImage);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Images";
+            pImage.ResumeLayout(false);
+            pImage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pbImage).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btImage;
+        private TextBox tbPath;
+        private ListBox libPath;
+        private Button btAdd;
+        private Button btClear;
+        private Button btDelete;
+        private Panel pImage;
+        private PictureBox pbImage;
     }
 }
--- 15Image(PictureBox)/Form1.cs
+++ 15Image(PictureBox)/Form1.cs
@@ -1,10 +1,73 @@
 namespace _15Image_PictureBox_
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
+
+        private void btImage_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Images(*.jpeg/*.jpg/*.png)|*.jpeg;*.jpg;*.png";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    tbPath.Text = filePath;
+                    if (libPath.Items.Count == 0)
+                    {
+                        btAdd.Enabled = true;
+                    }
+                    foreach (var item in libPath.Items)
+                    {
+                        if (item.ToString() != filePath)
+                        {
+                            btAdd.Enabled = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            libPath.Items.Add(tbPath.Text);
+            tbPath.Clear();
+            btClear.Enabled = true;
+            btAdd.Enabled = false;
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            libPath.Items.Remove(libPath.SelectedItem);
+            btDelete.Enabled = false;
+            if (libPath.Items.Count == 0)
+            {
+                btClear.Enabled = false;
+            }
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            libPath.Items.Clear();
+            btClear.Enabled = false;
+        }
+
+        private void libPath_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btDelete.Enabled = true;
+            pbImage.Load(libPath.SelectedItem.ToString());
+        }
+
     }
 }
