b76b3544cf1de4191cfb727ef6a3cb21cc6684cf
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-06 09:19:52 +0200

    exo 11 1.0

---
+++ 11Image/Form1.cs
@@ -0,0 +1,32 @@
+namespace _11Image
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Images(*.jpeg/*.jpg/*.png)|*.jpeg;*.jpg;*.png";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    tbPath.Text = filePath;
+                }
+            }
+
+        }
+    }
+}
