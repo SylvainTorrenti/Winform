191390ae708c9d260c2568432b6533ce6e21558e
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-07-20 09:26:15 +0200

    exo 15 1.4

--- 15Image(PictureBox)/Form1.cs
+++ 15Image(PictureBox)/Form1.cs
@@ -16,34 +16,7 @@ namespace _15Image_PictureBox_
         /// <param name="e"></param>
         private void btImage_Click(object sender, EventArgs e)
         {
-            var fileContent = string.Empty;
-            var filePath = string.Empty;
-
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "Images(*.jpeg/*.jpg/*.png)|*.jpeg;*.jpg;*.png";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    //Get the path of specified file
-                    filePath = openFileDialog.FileName;
-                    tbPath.Text = filePath;
-                    if (libPath.Items.Count == 0)
-                    {
-                        btAdd.Enabled = true;
-                    }
-                    foreach (var item in libPath.Items)
-                    {
-                        if (item.ToString() != filePath)
-                        {
-                            btAdd.Enabled = true;
-                        }
-                    }
-                }
-            }
+            Path();
         }
         #endregion
 
@@ -57,11 +30,12 @@ namespace _15Image_PictureBox_
         {
             if (tbPath.Text.Length > 0)
             {
-
+                // Ajout du chemin de l'image dans la libPath
                 libPath.Items.Add(tbPath.Text);
                 tbPath.Clear();
-                btClear.Enabled = true;
-                btAdd.Enabled = false;
+
+                // Appel de la fonction qui gère les grisages
+                GestionGrisages();
             }
         }
         #endregion
@@ -74,15 +48,13 @@ namespace _15Image_PictureBox_
         /// <param name="e"></param>
         private void btDelete_Click(object sender, EventArgs e)
         {
-            if (libPath.Items != null)
+            GestionGrisages();
+            if (libPath.SelectedIndex != -1)
             {
-
-                libPath.Items.Remove(libPath.SelectedItem);
+                // On supprime l'élément sélectionné dans la libPath
+                libPath.Items.RemoveAt(libPath.SelectedIndex);
                 btDelete.Enabled = false;
-            }
-            if (libPath.Items.Count == 0)
-            {
-                btClear.Enabled = false;
+                pbImage.Image = null;
             }
         }
         #endregion
@@ -96,7 +68,7 @@ namespace _15Image_PictureBox_
         private void btClear_Click(object sender, EventArgs e)
         {
             libPath.Items.Clear();
-            btClear.Enabled = false;
+            GestionGrisages();
         }
         #endregion
 
@@ -109,9 +81,63 @@ namespace _15Image_PictureBox_
         private void libPath_SelectedIndexChanged(object sender, EventArgs e)
         {
             btDelete.Enabled = true;
-            pbImage.Load(libPath.SelectedItem.ToString());
+            if (libPath.SelectedIndex != -1)
+            {
+
+                pbImage.Load(libPath.SelectedItem.ToString());
+                
+            }
         }
         #endregion
 
+        #region Fonction perso
+        private void GestionGrisages()
+        {
+
+            btAdd.Enabled = (tbPath.Text.Length > 0);
+
+            btDelete.Enabled = (libPath.SelectedIndex != -1);
+
+            btClear.Enabled = (libPath.Items.Count != 0);
+        }
+        private void Path()
+        {
+            string filePath = string.Empty;
+            string fileName = string.Empty;
+            string tempItemName = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Images(*.jpeg/*.jpg/*.png)|*.jpeg;*.jpg;*.png";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    fileName = System.IO.Path.GetFileName(filePath);
+                    tbPath.Text = filePath;
+                    if (tbPath.Text.Length > 0)
+                    {
+                        if (libPath.Items.Count == 0)
+                        {
+                            GestionGrisages();
+                        }
+                        foreach (var item in libPath.Items)
+                        {
+                            tempItemName = System.IO.Path.GetFileName(item.ToString());
+                            if (fileName != tempItemName)
+                            {
+                                GestionGrisages();
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
